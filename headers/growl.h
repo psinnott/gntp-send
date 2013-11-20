ae50dd6b22e7fc58e3b7d0c98d0d67517d5eb67f
Peter Sinnott <link@redbrick.dcu.ie>
2013-11-20 01:42:54 +0000

    Allow for distinct app and notification icons

--- headers/growl.h
+++ headers/growl.h
@@ -21,7 +21,7 @@ extern "C" {
 
 
 GROWL_EXPORT int growl( const char *const server,const char *const appname,const char *const notify,const char *const title, const char *const message ,
-                                const char *const icon , const char *const password , const char *url );
+                                const char *const icon , const char *const notification_icon , const char *const password , const char *url );
 GROWL_EXPORT int growl_tcp_notify( const char *const server,const char *const appname,const char *const notify,const char *const title, const char *const message ,
                                 const char *const password, const char* const url, const char* const icon );
 GROWL_EXPORT int growl_tcp_register( const char *const server , const char *const appname , const char **const notifications , const int notifications_count , const char *const password, const char *const icon );
